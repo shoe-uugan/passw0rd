dda4f85476f4ab2e78b4407f4e14f5501996e86a
25HP3408 25HP3408 <25HP3408@G301-CC0192.local>
2025-10-17 18:27:06 +0800

    commentz

--- src/app/comments/[id]/page.tsx
+++ src/app/comments/[id]/page.tsx
@@ -1,12 +1,15 @@
-
+"use client"
 import { CardDescription } from "@/components/ui/card";
 import { Textarea } from "@/components/ui/textarea";
 import { Bookmark, HatGlasses, Heart, Send, SendHorizonal, SendIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useContext, useEffect, useState } from "react";
 import { toast } from "sonner";
+import { UserContext } from "../../../../providers/UserProvider";
 
 type User = {
   username: string;
-};
+};        
 
 type Post = {
   createdBy: User;
@@ -14,16 +17,56 @@ type Post = {
   imageUrl: string;
   description: string;
 };
-  const CommentStuff = async ({ params }: { params: { id: string } }) => {
 
-    const { id } = params;
-    const response = await fetch("http://localhost:5500/posts/" + id);
-    const post: Post = await response.json();
-    console.log(post._id);
+  const CommentStuff = ({ params }: { params: { id: string } }) => {
+        const [data, setData] = useState(null);
+        const [description, setDescription] = useState("");
+        const router = useRouter();
+        const { token } = useContext(UserContext);
+    // const Comment = async({ params }: { params: { id: string } })=>{
+    
+      // const response = await fetch("http://localhost:5500/posts/" + id);
+      // const post: Post = await response.json();
+      // console.log(post._id);
 
-const PostComment = ()=>{
 
-}
+  useEffect(() => {
+    const Uhuh = async () => {
+      const { id } = await params;
+      try {
+        const response = await fetch(`http://localhost:5500/posts/` + id);
+        const post: Post = await response.json();
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+    Uhuh()
+  }, [params]);
+
+        const handleSubmit = async () => {
+          const response = await fetch("http://localhost:5500/comments", {
+            method: "POST",
+            body: JSON.stringify({ description, postId: params.id }),
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+              Authorization: "BEARER " + token,
+            },
+          });
+
+          const data = await response.json();
+          if (response.status !== 200) {
+            toast.error(data.message);
+            return;
+          }
+
+          toast.success("Post created successfully!");
+          router.push("/");
+        };
+
+    
     return (
       <div className="flex p-10 justify-center">
         <div className="flex flex-col">
@@ -50,8 +93,13 @@ const PostComment = ()=>{
             <SendHorizonal className="absolute right-5 bottom-5" />
           </div>
         </div>
+        <button onClick={handleSubmit}>aadcwdcde</button>
       </div>
+      
     );
+    
   };
-
+  
+  // return Comment
+ 
 export default CommentStuff;
