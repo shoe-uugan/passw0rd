1c5d0a645f095658bfa99b63f95a6183617eb077
25HP3408 25HP3408 <25HP3408@administrators-iMac.local>
2025-11-05 17:16:22 +0800

    avsgd

--- src/components/postCard.tsx
+++ src/components/postCard.tsx
@@ -49,7 +49,7 @@ export const PostCard = ({ post }: { post: Post }) => {
       <div className="flex justify-between">
         <Link href={`/${post.createdBy.username}`}>
           <Avatar>
-            <AvatarImage src="https://github.com/shadcn.png" />
+            <AvatarImage src="https://github.com/shadcn.png" className="rounded w-10 h-10" />
             <AvatarFallback>{post.createdBy.username}</AvatarFallback>
           </Avatar>
           <div className="font-bold text-[17px] pb-2">
